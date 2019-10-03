f2946b227b269aed1eec69c193e84fa2246b46cc
fahimferoje <fabrahamjohn@gmail.com>
2019-10-03 17:09:06 +0600

    sec 10 ep 70: del contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -56,7 +56,13 @@ const ContactState = (props) => {
 	};
 
 	//delete contact
-	const deleteContact = (id) => {
+	const deleteContact = async (id) => {
+		try {
+			const res = await axios.delete(`/api/contacts/${id}`);
+			dispatch({ type: DELETE_CONTACT, payload: id });
+		} catch (err) {
+			dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+		}
 		dispatch({ type: DELETE_CONTACT, payload: id });
 	};
 
